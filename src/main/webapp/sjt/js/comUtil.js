f07bacf4ba13aad90b14978fdbbe5dcc95b30819
cynthiamatthew <wangshulan@andoner.com>
2017-06-27 17:18:47 +0800

    问题详情跟进方和采集接口类型修改

--- src/main/webapp/sjt/js/comUtil.js
+++ src/main/webapp/sjt/js/comUtil.js
@@ -86,7 +86,7 @@ function codeUnique2(config){
  function loadCombobox(id , table,isAll){
 	 var time = (new Date().getTime());
 	 $.ajax({ 
-		 url: '/hdk/state/getSome',
+		 url: domainName + '/hdk/state/getSome',
 		 type:'get',
 		 data:{
 				'ownerTable':table,
